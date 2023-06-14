dba5dc845d2c3e5f42905f4e19515a5cf04fc9ee
EslebanPerez <eslebanperez@gmail.com>
2023-06-13 20:54:06 -0600

    Agregando un servicio con nest g s cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get,Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-    private carlist=['Toyota', 'Honda', 'Jeep'];
     @Get()
     getAllCars(){
         return this.carlist;
