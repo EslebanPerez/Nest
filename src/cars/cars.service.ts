6425a92318824bff8c8dcab7c0c34a3f198a40d9
EslebanPerez <eslebanperez@gmail.com>
2023-06-14 22:21:45 -0600

    Creando método para agregar un nuevo registro

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid} from 'uuid'
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -29,6 +30,18 @@ export class CarsService {
     public findOnebyId(id:string){
         const car = this.cars.find(car => car.id===id);
         if(!car) throw new NotFoundException(`Car with id '${id}' not found`)
+        return car;
+    }
+
+    public create( CreateCarDto: CreateCarDto){
+         //const { brand, model } = CreateCarDto
+        // const car = this.cars.push({id: uuid(), brand, model})
+        // return car;
+        const car:Car={
+            id: uuid(),
+            ...CreateCarDto
+        }
+        this.cars.push(car);
         return car;
     }
 
